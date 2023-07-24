537b5878ca7a740bedf3f4492f05a0c446651f9a
Igor Stepanov <stipyk1309@gmail.com>
2023-07-24 16:25:16 +0300

    Workout, Running and Cycling Classes implement

--- script.js
+++ script.js
@@ -12,6 +12,51 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date().toDateString();
+  id = Date.now();
+
+  constructor(distance, duration, coords) {
+    this.coords = coords; //[lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(distance, duration, coords);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(distance, duration, coords);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / this.duration / 60;
+    this.speed;
+  }
+}
+
+// const running1 = new Running([39, -12], 5.2, 23, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(running1, cycling1);
+
+//APPLICATION ARCHITECTURE
+
 class App {
   #map;
   #mapEvent;
@@ -94,7 +139,4 @@ class App {
 }
 
 const app = new App();
-
-
-
 
