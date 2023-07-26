f2c2192ab8f6ce9636468a789b54f0355e79c89f
Igor Stepanov <stipyk1309@gmail.com>
2023-07-27 00:48:51 +0300

    work with LocalStorage

--- script.js
+++ script.js
@@ -79,7 +79,13 @@ class App {
   #zoomLevel = 13;
 
   constructor() {
+    // Get user position
     this._getPosition();
+
+    //Get data from locale storage
+    this._getLocaleStorage();
+
+    // Attach event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -114,6 +120,10 @@ class App {
 
     // Handling click on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -184,7 +194,6 @@ class App {
 
     // Add new object to workout array
     this.#workouts.push(workout);
-    // console.log(this.#workouts);
 
     // Render workout on map as marker
     this._renderWorkoutMarker(workout);
@@ -194,6 +203,9 @@ class App {
 
     // Hide form + Clear input fields
     this._hideForm();
+
+    //Set local storage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -277,7 +289,6 @@ class App {
     const workout = this.#workouts.find(
       work => work.id == workoutEl.dataset.id
     );
-
     this.#map.setView(workout.coords, this.#zoomLevel, {
       animate: true,
       pan: {
@@ -286,9 +297,29 @@ class App {
     });
 
     //example how to use public interface
-    workout.click();
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocaleStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
 const app = new App();
-
